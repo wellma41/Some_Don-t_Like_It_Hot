59d66d7fd37dfdf490ebfa6d1d97b0f03111c6aa
Luke S <goathaho9@gmail.com>
2022-02-03 20:03:25 -0500

    Expanded table schemas

--- DB_Data/DB_Schema.sql
+++ DB_Data/DB_Schema.sql
@@ -13,3 +13,27 @@ CREATE TABLE Clean_Divorce_Data (
 
 SELECT * FROM Clean_Divorce_Data;
 
+CREATE TABLE Temperature_Data (
+	FIPS_Code varchar(8) NOT NULL,
+	Year INT NOT NULL,
+	AVG_Temp REAL NOT NULL,
+	MAX_Temp REAL NOT NULL,
+	MIN_Temp REAL NOT NULL,
+	PRIMARY KEY(FIPS_Code)
+);
+
+CREATE TABLE Precipitation_Data (
+	FIPS_Code varchar(8) NOT NULL,
+	Year INT NOT NULL,
+	AVG_Precip REAL NOT NULL,
+	MAX_Precip REAL NOT NULL,
+	MIN_Precip REAL NOT NULL,
+	PRIMARY KEY(FIPS_Code)
+);
+
+CREATE TABLE Raw_Divorce_Data (
+	FIPS_Code varchar(8) NOT NULL,
+	Year INT NOT NULL,
+	Div_Rate REAL NOT NULL,
+	PRIMARY KEY(FIPS_Code)
+);
